6ee030e19c09da9abef8f6e2d28609bf4dd7bc1f
Matt Ramotar <matt.ramotar@jhu.edu>
2020-11-21 15:41:07 -0800

    Add UserType schema

---
+++ backend/server/schema/UserType.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const graphql = require('graphql');
+
+const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLList, GraphQLBoolean, GraphQLInt } = graphql;
+
+const User = mongoose.model('User');
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+
+  fields: () => ({
+    id: { type: GraphQLID },
+    firstName: { type: GraphQLString },
+    lastName: { type: GraphQLString },
+    email: { type: GraphQLString },
+
+    googleId: { type: GraphQLString },
+    picture: { type: GraphQLString },
+  }),
+});
+
+module.exports = UserType;
