ecc050e4996e05730b4ac27c3f87137260a5a05b
Matt Ramotar <matt.ramotar@jhu.edu>
2020-11-22 14:44:09 -0800

    Add accounts associated with item

--- backend/services/item.js
+++ backend/services/item.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
+const getAccounts = require('../helpers/getAccounts');
 
 const User = mongoose.model('User');
 const Item = mongoose.model('Item');
+const Account = mongoose.model('Account');
 
 const addItem = async data => {
   try {
+    console.log(data);
     const { userId, itemId, accessToken } = data;
 
     const user = await User.findById(userId);
@@ -15,11 +18,17 @@ const addItem = async data => {
       accessToken,
     });
 
-    console.log(user);
-
     await user.items.push(item);
+
+    const accounts = await getAccounts(accessToken);
+
+    for (const account of accounts) {
+      const { account_id, mask, official_name, subtype, type } = account;
+      const newAccount = await Account.create({ _id: account_id, mask, officialName: official_name, subtype, type });
+      await user.accounts.push(newAccount);
+    }
+
     await user.save();
-    console.log(user);
 
     return { _id: item.id, user, accessToken };
   } catch (err) {
