b35518acdd75ba9daeab22f6d3f67da9d6c565ea
Matt Ramotar <matt.ramotar@jhu.edu>
2020-11-21 17:08:04 -0800

    Configure Apollo

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import App from './App';
 import 'fontsource-roboto';
 
+const link = createHttpLink({ uri: 'http://localhost:5000/graphql' });
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  cache,
+  link,
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
