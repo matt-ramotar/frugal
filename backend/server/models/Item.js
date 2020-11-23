adbe012f455eabbc82e884eae18940d3476b1b71
Matt Ramotar <matt.ramotar@jhu.edu>
2020-11-23 01:04:56 -0800

    Revise Item model

--- backend/server/models/Item.js
+++ backend/server/models/Item.js
@@ -8,7 +8,6 @@ const ItemSchema = new Schema({
   plaidItemId: { type: String },
   user: { type: Schema.Types.ObjectId, ref: 'User' },
   accessToken: { type: String },
-  institution: { type: Schema.Types.ObjectId, ref: 'Institution' },
   accounts: [{ type: Schema.Types.ObjectId, ref: 'Account' }],
 });
 
